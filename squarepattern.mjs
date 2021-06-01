27d2e49fd4f7b52a4de640f7fac5c8d05add08b3
Somukhannan Thiyagarajan <s.thiyagarajan@lakeba.com>
2021-06-01 19:12:28 +0530

    Middle line improved.

--- squarepattern.mjs
+++ squarepattern.mjs
@@ -1,11 +1,8 @@
-const charac = (arr, val) => '' + arr[val - 1];
-
 const middleLines = function (arr) {
-  var count = arr.length - 2;
-  var result = '';
-  for (var i = 0; i < count; i++) {
-    var val = (arr.length - 1) - i;
-    result += arr[i + 1] + ' '.repeat(count) + charac(arr,val)+'\n';
+  const count = arr.length;
+  let result = '';
+  for (let i = 0; i < count; i++) {
+    result += arr[i] + ' '.repeat(count) + '' + arr[count - 1 - i]+'\n';
   }
   return result;
 }
@@ -13,7 +10,8 @@ const middleLines = function (arr) {
 const firstLine = (arr) =>arr.reduce((i,c) => i+c)+'\n';
 
 const square = (arr) => {
-  return firstLine(arr) + middleLines(arr) + firstLine(arr.reverse());
+  return firstLine(arr) + middleLines(arr.slice(1,-1)) + firstLine(arr.slice().reverse());
+  
 }
 console.log(square(['a', 'b']));
 console.log(square(['a', 'b', 'c']));
