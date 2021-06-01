63dc369558b7359c393bdb2b73a492d48e6bb61d
Somukhannan Thiyagarajan <s.thiyagarajan@lakeba.com>
2021-06-01 19:49:20 +0530

    modified using map

--- squarepattern.mjs
+++ squarepattern.mjs
@@ -1,18 +1,16 @@
+const middleline = (val,index,arr) => val + ' '.repeat(arr.length) + arr[arr.length - 1 - index]+'\n';
+
 const middleLines = function (arr) {
-  const count = arr.length;
-  let result = '';
-  for (let i = 0; i < count; i++) {
-    result += arr[i] + ' '.repeat(count) + '' + arr[count - 1 - i]+'\n';
-  }
-  return result;
+  const lines = arr.map(middleline);
+  return lines.join('');
 }
 
 const firstLine = (arr) =>arr.reduce((i,c) => i+c)+'\n';
 
 const square = (arr) => {
   return firstLine(arr) + middleLines(arr.slice(1,-1)) + firstLine(arr.slice().reverse());
-  
 }
+
 console.log(square(['a', 'b']));
 console.log(square(['a', 'b', 'c']));
 console.log(square(['a', 'b', 'c', 'd','e']));
