05ba366ce086bb1ea7a0e794628deb8ea9828a40
Somukhannan Thiyagarajan <s.thiyagarajan@lakeba.com>
2021-06-01 20:29:48 +0530

    Changed the code to readability.

--- squarepattern.mjs
+++ squarepattern.mjs
@@ -1,16 +1,22 @@
-const middleline = (val,index,arr) => val + ' '.repeat(arr.length) + arr[arr.length - 1 - index]+'\n';
+const middleLine = () => 
+(val, index, arr) => val + ' '.repeat(arr.length) + arr[arr.length - 1 - index];
 
-const middleLines = function (arr) {
-  const lines = arr.map(middleline);
-  return lines.join('');
-}
+const middleLines = (arr) => arr.map(middleLine());
 
-const firstLine = (arr) =>arr.reduce((i,c) => i+c)+'\n';
+const firstLine = (arr) => arr.join('')
 
+const lastLine = (arr) => arr.slice().reverse().join('');
+  
 const square = (arr) => {
-  return firstLine(arr) + middleLines(arr.slice(1,-1)) + firstLine(arr.slice().reverse());
+  const lines = [
+    firstLine(arr),
+    ...middleLines(arr.slice(1,-1)),
+    lastLine(arr)
+  ].join('\n')+'\n';
+  return lines;
 }
 
 console.log(square(['a', 'b']));
 console.log(square(['a', 'b', 'c']));
 console.log(square(['a', 'b', 'c', 'd','e']));
+
